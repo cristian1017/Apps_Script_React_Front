b24ef38381b46dad5c9197934f72c6811869a7f8
Cristian Gutierrez <cristianmauriciogutierrez1998@gmail.com>
2023-02-22 23:11:31 -0500

    Se crea front mediante app's script

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 export const App = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    google.script.run
+      .withSuccessHandler((resp) => {
+        setData(JSON.parse(resp));
+      })
+      .getDataJSON();
+  }, [])
+
   return (
     <div>
-      <h1>Hola como estas el dia de hoy?</h1>
+      <h1>Hola como estas el dia de hoy ?</h1>
+      {data.map((e)=> {
+        console.log(e)
+      })}
     </div>
-  )
+  );
 }
